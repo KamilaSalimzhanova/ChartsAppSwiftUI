181a3793572117e7c2e0c10a7443a40859d12049
kamila <ksalimzh@gmail.com>
2025-03-30 13:44:07 +0500

    Initial Commit

---
+++ ChartsAppSwiftUI/ChartsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsAppSwiftUIApp.swift
+//  ChartsAppSwiftUI
+//
+//  Created by kamila on 30.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChartsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
